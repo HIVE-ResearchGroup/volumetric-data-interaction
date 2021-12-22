cf8b8f2e743861faaead8672355f8ac2803afb1a
Local <Local@HAGULABPC04>
2021-12-22 15:05:06 +0100

    Fix Network connection for combination of PC and Tablet

--- Assets/Scripts/ConfigurationConstants.cs
+++ Assets/Scripts/ConfigurationConstants.cs
@@ -1,7 +1,6 @@
 ﻿public static class ConfigurationConstants
 {
     public const string DEFAULT_IP = "127.0.0.1";
-    public const string DEFAULT_CONNECTING_PORT = "7777";
-    public const string DEFAULT_HOSTING_PORT = "7777";
-    public const string DEFAULT_WEBSOCKET_PORT = "7778";
+    public const string HOST_IP = "10.42.1.26"; // IP Adress of PC
+    public const int DEFAULT_CONNECTING_PORT = 7777;
 }
--- Assets/Scripts/ConnectionManager.cs
+++ Assets/Scripts/ConnectionManager.cs
@@ -1,13 +1,19 @@
 ﻿using MLAPI;
 using MLAPI.Transports.UNET;
 using UnityEngine;
+using UnityEngine.UI;
 
+/// <summary>
+/// Firewall for Domain Network needs to be deactivated!
+/// </summary>
 public class ConnectionManager : MonoBehaviour
 {
 
     public GameObject connectionButtonPanel;
+    public Text log; 
 
     private UnetTransport transport;
+
     /// <summary>
     /// Server only
     /// </summary>
@@ -16,13 +22,17 @@ public class ConnectionManager : MonoBehaviour
         connectionButtonPanel.SetActive(false);
 
         transport = NetworkingManager.Singleton.GetComponent<UnetTransport>();
-        transport.ConnectAddress = ConfigurationConstants.DEFAULT_IP;
+        transport.ConnectAddress = ConfigurationConstants.HOST_IP;
+        transport.ConnectPort = ConfigurationConstants.DEFAULT_CONNECTING_PORT;
+
         NetworkingManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
         NetworkingManager.Singleton.StartHost(GetRandomSpawn(), Quaternion.identity);
+
+        log.text += $"-HOST {transport.ConnectAddress}:{transport.ConnectPort}";
     }
 
     /// <summary>
-    /// server only
+    /// server only, PC
     /// </summary>
     private void ApprovalCheck(byte[] connectionData, ulong clientID, NetworkingManager.ConnectionApprovedDelegate callback)
     {
@@ -30,14 +40,20 @@ public class ConnectionManager : MonoBehaviour
         callback(true, null, true, GetRandomSpawn(), Quaternion.identity);
     }
 
+    /// <summary>
+    /// Used for Tablet to connect
+    /// </summary>
     public void Join()
     {
+        var ipaddress = System.Net.Dns.GetHostAddresses("")[0];
+
         transport = NetworkingManager.Singleton.GetComponent<UnetTransport>();
-        transport.ConnectAddress = ConfigurationConstants.DEFAULT_IP;
-        //transport.ConnectPort = ConfigurationConstants.DEFAULT_CONNECTING_PORT;
+        transport.ConnectAddress = ConfigurationConstants.HOST_IP;
+        transport.ConnectPort = ConfigurationConstants.DEFAULT_CONNECTING_PORT;
 
         connectionButtonPanel.SetActive(false);
         NetworkingManager.Singleton.StartClient();
+        log.text += $"-CLIENT {transport.ConnectAddress}:{transport.ConnectPort}";
     }
 
     private Vector3 GetRandomSpawn()
