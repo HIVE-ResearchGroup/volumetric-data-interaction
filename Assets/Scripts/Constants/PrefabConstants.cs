c6943b351abd5682940d88097c6e27a29a8d9951
Local <Local@HAGULABPC04>
2022-02-27 14:42:34 +0100

    Move Prefabs to Resources

--- Assets/Scripts/Constants/PrefabConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿/// <summary>
-/// Static Class Holding all prefab paths
-/// </summary>
-public static class PrefabConstants
-{
-    public static string SECTION_MODEL_CUBE = "Assets/Prefabs/CrossSection/SectionModel.prefab";
-    public static string SECTION_QUAD = "Assets/Prefabs/CrossSection/SectionQuad.prefab";
-}
