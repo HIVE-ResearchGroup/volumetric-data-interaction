19053018dd93974b6cd72d5b9aff0d9e9c0956f0
Janine <s2010745015@fh-ooe.at>
2022-06-22 14:46:31 +0900

    Add scripts for permantent object slicing

---
+++ Assets/Scripts/Exploration/SliceListener.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Exploration
+{
+    /// <summary>
+    /// https://github.com/LandVr/SliceMeshes
+    /// </summary>
+    public class SliceListener : MonoBehaviour
+    {
+        public Slicer slicer;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            slicer.isTouched = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Exploration/Slicer.cs
@@ -0,0 +1,58 @@
+﻿using EzySlice;
+using UnityEngine;
+
+namespace Assets.Scripts.Exploration
+{
+    /// <summary>
+    /// https://github.com/LandVr/SliceMeshes
+    /// </summary>
+    public class Slicer : MonoBehaviour
+    {
+        public Material materialAfterSlice;
+        public LayerMask sliceMask;
+        public bool isTouched;
+
+        private void Update()
+        {
+            if (isTouched)
+            {
+                SliceObject();
+            }
+        }
+
+
+        private void SliceObject()
+        {
+            isTouched = false;
+
+            Collider[] objectsToBeSliced = Physics.OverlapBox(transform.position, new Vector3(1, 0.1f, 0.1f), transform.rotation, sliceMask);
+
+            foreach (Collider objectToBeSliced in objectsToBeSliced)
+            {
+                SlicedHull slicedObject = SliceObject(objectToBeSliced.gameObject, materialAfterSlice);
+
+                GameObject upperHullGameobject = slicedObject.CreateUpperHull(objectToBeSliced.gameObject, materialAfterSlice);
+                GameObject lowerHullGameobject = slicedObject.CreateLowerHull(objectToBeSliced.gameObject, materialAfterSlice);
+
+                upperHullGameobject.transform.position = objectToBeSliced.transform.position;
+                lowerHullGameobject.transform.position = objectToBeSliced.transform.position;
+
+                MakeItPhysical(upperHullGameobject);
+                MakeItPhysical(lowerHullGameobject);
+
+                Destroy(objectToBeSliced.gameObject);
+            }
+        }
+
+        private void MakeItPhysical(GameObject obj)
+        {
+            obj.AddComponent<MeshCollider>().convex = true;
+            obj.AddComponent<Rigidbody>();
+        }
+
+        private SlicedHull SliceObject(GameObject obj, Material crossSectionMaterial = null)
+        {
+            return obj.Slice(transform.position, transform.up, crossSectionMaterial);
+        }
+    }
+}
