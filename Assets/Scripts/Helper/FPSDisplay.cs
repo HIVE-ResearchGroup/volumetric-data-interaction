118b2b5c07a4866b95bfef23669831003f80f795
Janine <s2010745015@fh-ooe.at>
2022-08-02 16:54:06 +0900

    Add FPS script

---
+++ Assets/Scripts/Helper/FPSDisplay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FPSDisplay : MonoBehaviour
+{
+    public float FPS;
+
+    void Update()
+    {
+        float current = (int)(1f / Time.deltaTime);
+        if (Time.frameCount % 50 == 0)
+        {
+            FPS = current;
+        }
+    }
+}
