45fac52b7a182f663ae59997cd103a0a8c791967
P42542 <P42542@HIVE-LAB-PC-2>
2022-10-11 13:59:15 +0200

    Update Ipaddress

--- Assets/Scripts/Constants/ConfigurationConstants.cs
+++ Assets/Scripts/Constants/ConfigurationConstants.cs
@@ -1,7 +1,7 @@
 ﻿public static class ConfigurationConstants
 {
     public const string DEFAULT_IP = "127.0.0.1";
-    public const string HOST_IP = "10.42.1.54"; // IP Adress of PC
+    public const string HOST_IP = "10.24.99.77"; // IP Adress of PC
     public const int DEFAULT_CONNECTING_PORT = 7777;
 
     public const int SNAPSHOT_DISTANCE = 2;
