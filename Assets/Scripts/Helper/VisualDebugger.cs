d2ca68d34dac4c1748bd9e23a10ef2f738547406
jdmayer <mayer.janine.denise@gmail.com>
2022-10-25 15:24:50 +0200

    Add visual debugger Helper with methods for debug vis

---
+++ Assets/Scripts/Helper/VisualDebugger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Helper
+{
+    public static class VisualDebugger
+    {
+        public static GameObject CreateDebugPrimitive(Vector3 position, Color colour, float size = 0.01f, string name = "primitive")
+        {
+            var primitive = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            primitive.transform.position = position;
+            primitive.transform.localScale = new Vector3(size, size, size);
+            primitive.GetComponent<MeshRenderer>().material.color = colour;
+            primitive.name = name;
+            return primitive;
+        }
+    }
+}
