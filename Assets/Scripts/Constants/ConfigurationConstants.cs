9b83db462204e487834a2b2a772ca58dfbe3751e
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2023-03-20 10:55:21 +0100

    updated constants

--- Assets/Scripts/Constants/ConfigurationConstants.cs
+++ Assets/Scripts/Constants/ConfigurationConstants.cs
@@ -8,7 +8,9 @@
     public const int NEIGHBOUR_DISTANCE = 5; // pixel, except for if it is along x-axis, then it is slices
     public const int BLACK_TRANSPARENT_THRESHOLD = 30;
 
-    private const string DATA_FOLDER_PATH = @"C:\Users\P42542\Desktop\Data\";
+    private const string DATA_FOLDER_PATH = @"C:\Users\p42652\Desktop\Janine_VIVE_Data\Data\";
+
+    // all paths from here are unused!
     public const string X_STACK_PATH_LOW_RES = DATA_FOLDER_PATH + @"Stack_x_0.2mm_lowRes";
     public const string X_STACK_PATH = DATA_FOLDER_PATH + @"Nähmaschine\Stack_x_1mm";
     public const string Y_STACK_PATH = DATA_FOLDER_PATH + @"Stack_y_1mm";
