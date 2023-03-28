ee6c1471dfd13b16121f63460833216b284d62dc
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2023-03-28 15:00:28 +0200

    created an IEnumerable extension to call foreach

---
+++ Assets/Scripts/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var item in enumerable)
+            {
+                action(item);
+            }
+        }
+    }
+}
