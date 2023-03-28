91895ec0954a3790b501c84e16c5b924e5b309c9
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2023-03-28 15:23:45 +0200

    reworked FPSDisplay to FPSCounter

---
+++ Assets/Scripts/Helper/FPSCounter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Helper
+{
+    public class FPSCounter : MonoBehaviour
+    {
+        public int FPS;
+
+        void Update()
+        {
+            FPS = (int)(1f / Time.deltaTime);
+        }
+    }
+}
