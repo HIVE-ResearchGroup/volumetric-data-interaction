a2ee45e7d25c53fb1b2b1b58dce2313f69fb9d6a
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2023-06-06 09:28:17 +0200

    NetworkConfigHandler: use the TMP InputField instead of directly accessing the TMP Text, as the input field does some further cleaning of the string (there is a zero with character in the text field)

--- Assets/Scripts/Interaction/NetworkConfigHandler.cs
+++ Assets/Scripts/Interaction/NetworkConfigHandler.cs
@@ -8,15 +8,23 @@ namespace Interaction
     public class NetworkConfigHandler : MonoBehaviour
     {
         [SerializeField]
-        private TMP_Text ipText;
+        private TMP_InputField ipText;
 
         [SerializeField]
         private NetworkManager netMan;
 
+        private UnityTransport _unityTransport;
+
+        private void Awake()
+        {
+            _unityTransport = netMan.GetComponent<UnityTransport>();
+        }
+
         public void TryReconnect()
         {
             var ip = ipText.text;
-            netMan.GetComponent<UnityTransport>().ConnectionData.Address = ip;
+            Debug.Log($"IP entered: {ip}");
+            _unityTransport.ConnectionData.Address = ip;
             netMan.StartClient();
         }
     }
