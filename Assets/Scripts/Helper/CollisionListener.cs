b1134bbb5fb3ffa8e3bef731ba6a65888362e818
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2023-06-12 08:21:34 +0200

    CollisionListener: added remove methods

--- Assets/Scripts/Helper/CollisionListener.cs
+++ Assets/Scripts/Helper/CollisionListener.cs
@@ -9,38 +9,50 @@ namespace Helper
         private event Action<Collider> OnCollisionEnter;
         private event Action<Collider> OnCollisionExit;
 
-        private List<Action<Collider>> enterListeners = new List<Action<Collider>>();
-        private List<Action<Collider>> exitListeners = new List<Action<Collider>>();
+        private readonly List<Action<Collider>> _enterListeners = new();
+        private readonly List<Action<Collider>> _exitListeners = new();
 
         private void OnTriggerEnter(Collider other)
         {
-            OnCollisionEnter(other);
+            OnCollisionEnter?.Invoke(other);
         }
 
         private void OnTriggerExit(Collider other)
         {
-            OnCollisionExit(other);
+            OnCollisionExit?.Invoke(other);
         }
 
         public void AddEnterListener(Action<Collider> listener)
         {
-            enterListeners.Add(listener);
+            _enterListeners.Add(listener);
             OnCollisionEnter += listener;
         }
 
         public void AddExitListener(Action<Collider> listener)
         {
-            exitListeners.Add(listener);
+            _exitListeners.Add(listener);
             OnCollisionExit += listener;
+        }
+
+        public void RemoveEnterListener(Action<Collider> listener)
+        {
+            _enterListeners.Remove(listener);
+            OnCollisionEnter -= listener;
+        }
+
+        public void RemoveExitListener(Action<Collider> listener)
+        {
+            _exitListeners.Remove(listener);
+            OnCollisionExit -= listener;
         }
 
         private void OnDestroy()
         {
-            foreach (var listener in enterListeners)
+            foreach (var listener in _enterListeners)
             {
                 OnCollisionEnter -= listener;
             }
-            foreach (var listener in exitListeners)
+            foreach (var listener in _exitListeners)
             {
                 OnCollisionExit -= listener;
             }
