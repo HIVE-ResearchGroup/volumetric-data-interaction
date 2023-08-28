7d02774f95cbd565f4230d7468183f4080d49c16
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2023-08-28 11:40:55 +0200

    SpatialInteraction: removed transform ability as it will be removed later on

--- Assets/Scripts/Interaction/SpatialInteraction.cs
+++ Assets/Scripts/Interaction/SpatialInteraction.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Interaction
 {
@@ -13,23 +14,48 @@ namespace Interaction
 
         private bool _updateStartRotation;
 
+        //private Vector3 _startInputTransform;
+        private Vector3 _startTransform;
+        private Vector3 _calibrationResult;
+        private Vector3 _velocity = Vector3.zero;
+        private Vector3 _accumulatedTransformation;
+        private bool _transformMapping;
+
+        private bool _updateStartTransform;
+
         public bool RotationMapping
         {
             get => _rotationMapping;
             set
             {
-                if (value != _rotationMapping)
+                if (value == _rotationMapping)
                 {
-                    _rotationMapping = value;
-                    if (_rotationMapping)
-                    {
-                        _updateStartRotation = true;
-                    }
+                    return;
+                }
+                _rotationMapping = value;
+                if (_rotationMapping)
+                {
+                    _updateStartRotation = true;
                 }
             }
         }
-    
-        public bool TransformMapping { get; set; }
+
+        public bool TransformMapping
+        {
+            get => _transformMapping;
+            set
+            {
+                if (value == _transformMapping)
+                {
+                    return;
+                }
+                _transformMapping = value;
+                if (_transformMapping)
+                {
+                    _updateStartTransform = true;
+                }
+            }
+        }
 
         /// <summary>
         /// Execute rotation depending on tracker orientation 
@@ -65,16 +91,16 @@ namespace Interaction
         public void HandleRotation(Quaternion rotation, GameObject selectedObject)
         {
             if (!RotationMapping ||
-                selectedObject is null)
+                selectedObject == null)
             {
                 return;
             }
 
             if (_updateStartRotation)
             {
+                _updateStartRotation = false;
                 _startRotation = selectedObject.transform.rotation;
                 _startInputRotation = rotation;
-                _updateStartRotation = false;
             }
 
             Debug.Log($"Rotation: {rotation}");
@@ -83,14 +109,46 @@ namespace Interaction
 
         public void HandleTransform(Vector3 transformation, GameObject selectedObject)
         {
-            if (!TransformMapping ||
-                selectedObject is null)
+            /*if (!TransformMapping ||
+                selectedObject == null)
             {
                 return;
             }
-            
-            Debug.Log($"Transformation: {transformation}");
-            selectedObject.transform.Translate(transformation, Space.World);
+
+            if (_updateStartTransform)
+            {
+                _updateStartTransform = false;
+                _startTransform = selectedObject.transform.position;
+                //_startInputTransform = transformation;
+                _velocity = Vector3.zero;
+                _calibrationResult = transformation;
+                _accumulatedTransformation = Vector3.zero;
+            }
+
+            // 1) get acceleration
+            // 2) get current velocity
+            // 3) apply velocity to get a new position
+            // keep it relatively smooth, as the sensors are twitchy
+            var acceleration = transformation - _calibrationResult;
+            if (acceleration.magnitude <= 0.01f)
+            {
+                acceleration = Vector3.zero;
+            }
+            _velocity += acceleration;
+            if (_velocity.magnitude <= 0.01f)
+            {
+                _velocity = Vector3.zero;
+            }
+            _accumulatedTransformation += _velocity;
+            _accumulatedTransformation.x = Mathf.Round(_accumulatedTransformation.x * 100.0f) / 100.0f;
+            _accumulatedTransformation.y = Mathf.Round(_accumulatedTransformation.y * 100.0f) / 100.0f;
+            _accumulatedTransformation.z = Mathf.Round(_accumulatedTransformation.z * 100.0f) / 100.0f;
+            Debug.Log($"Input Transformation: {transformation}");
+            Debug.Log($"Velocity: {_velocity}");
+            Debug.Log($"Accumulated Transformation: {_accumulatedTransformation}");
+            selectedObject.transform.position = _startTransform + _accumulatedTransformation;
+            //selectedObject.transform.Translate(transformation, Space.World);
+            */
         }
 
         /*public void StartMapping(GameObject selectedObject)
