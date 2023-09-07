2fde27c68fb0aace0402b76b1818f592691f2b51
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2023-09-07 19:35:26 +0200

    added editor extension to test selection without VR headsets

---
+++ Assets/Scripts/Interaction/SelectableEditor.cs
@@ -0,0 +1,35 @@
+using Networking;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace Interaction
+{
+    [CustomEditor(typeof(Selectable))]
+    public class SelectableEditor : Editor
+    {
+        private SerializedProperty _hostProp;
+        private MethodInfo _method;
+
+        private void OnEnable()
+        {
+            _hostProp = serializedObject.FindProperty("host");
+            _method = typeof(Host).GetMethod("HandleTap", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Select"))
+            {
+                Host host = (Host)_hostProp.objectReferenceValue;
+                _method.Invoke(host, new object[] { TapType.Double, 0.0f, 0.0f });
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
