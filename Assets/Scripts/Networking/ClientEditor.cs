5e55c9bddd2e36cc962802e0094a4f15c45a3fdd
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2023-09-12 14:05:14 +0200

    ClientEditor: added client editor extension to trigger menu changes and send basic touch commands

---
+++ Assets/Scripts/Networking/ClientEditor.cs
@@ -0,0 +1,47 @@
+using Interaction;
+using UnityEditor;
+using UnityEngine;
+
+namespace Networking
+{
+    [CustomEditor(typeof(Client))]
+    public class ClientEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var client = (Client)target;
+
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Send Menu: Analysis"))
+            {
+                client.SendMenuChangedMessage(MenuMode.Analysis);
+            }
+
+            if (GUILayout.Button("Send Menu: Selection"))
+            {
+                client.SendMenuChangedMessage(MenuMode.Selection);
+            }
+
+            if (GUILayout.Button("Send Menu: Mapping"))
+            {
+                client.SendMenuChangedMessage(MenuMode.Mapping);
+            }
+
+            if (GUILayout.Button("Send Swipe"))
+            {
+                client.SendSwipeMessage(true, 250, 250, 0);
+            }
+
+            if (GUILayout.Button("Send Shake"))
+            {
+                client.SendShakeMessage(3);
+            }
+
+            if (GUILayout.Button("Send Double Tap"))
+            {
+                client.SendTapMessage(TapType.Double, 250, 250);
+            }
+        }
+    }
+}
