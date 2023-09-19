6c66e7f81cffba48a627565d3cd7efb7c19e839c
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2023-09-19 10:06:09 +0200

    TabletOverlay: set a fixed number of additions because in the same transform non-additions are placed

--- Assets/Scripts/Snapshots/TabletOverlay.cs
+++ Assets/Scripts/Snapshots/TabletOverlay.cs
@@ -5,16 +5,18 @@ namespace Snapshots
 {
     public class TabletOverlay : MonoBehaviour
     {
+        private const int AdditionCount = 5;
+        
         public Transform Main { get; private set; }
 
-        public List<Transform> Additions { get; } = new();
+        public List<Transform> Additions { get; } = new(AdditionCount);
 
         private void Awake()
         {
             // the first one is main
             // get all additions and add them to the list
             Main = transform.GetChild(0);
-            for (var i = 0; i < transform.childCount - 1; i++)
+            for (var i = 0; i < AdditionCount; i++)
             {
                 Additions[i] = transform.GetChild(i + 1);
             }
