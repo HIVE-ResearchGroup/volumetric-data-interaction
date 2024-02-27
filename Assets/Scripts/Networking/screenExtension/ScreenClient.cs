dcf642759042ec65f3578bbf6096bb553c7995b3
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2024-02-27 13:24:52 +0100

    first version of screen server and client

--- Assets/Scripts/Networking/screenExtension/ScreenClient.cs
+++ Assets/Scripts/Networking/screenExtension/ScreenClient.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Linq;
 using System.Net.Sockets;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Networking.screenExtension
 {
@@ -11,17 +14,39 @@ namespace Networking.screenExtension
         [SerializeField]
         private short port = 8642;
 
+        [SerializeField]
+        private int id = 1;
+
+        [SerializeField]
+        private Image image;
+
         private async void Start()
         {
             using var client = new TcpClient();
             await client.ConnectAsync(ip, port);
             await using var stream = client.GetStream();
 
-            var buffer = new byte[1024];
+            var bytes = BitConverter.GetBytes(id);
+            await stream.WriteAsync(bytes, 0, bytes.Length);
+
+            var buffer = new Memory<byte>();
             while (await stream.ReadAsync(buffer) > 0)
             {
-                // TODO form image
+                var tex = new Texture2D(2, 2);
+                var colors = new Color32[buffer.Length / 4];
+                var bufferArray = buffer.Span.ToArray();
+                for (var i = 0; i < colors.Length; i++)
+                {
+                    colors[i].r = bufferArray[i * 4];
+                    colors[i].g = bufferArray[i * 4 + 1];
+                    colors[i].b = bufferArray[i * 4 + 2];
+                    colors[i].a = bufferArray[i * 4 + 3];
+                }
+                tex.SetPixels32(colors);
+                image.material.mainTexture = tex;
             }
+            
+            Debug.LogWarning("Client loop has stopped!");
         }
     }
 }
--- Assets/Scripts/Networking/screenExtension/ScreenServer.cs
+++ Assets/Scripts/Networking/screenExtension/ScreenServer.cs
@@ -1,9 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Networking.screenExtension
 {
     public class ScreenServer : MonoBehaviour
     {
+        [SerializeField]
+        private int port = 8642;
         
+        private TcpListener _server;
+
+        private readonly Dictionary<int, TcpClient> _clients = new();
+
+        private void Awake()
+        {
+            _server = new TcpListener(IPAddress.Loopback, port);
+        }
+
+        private async void Start()
+        {
+            _server.Start();
+            Debug.Log($"Screen server started on port {port}.");
+
+            while (true)
+            {
+                var client = await _server.AcceptTcpClientAsync();
+                await using var stream = client.GetStream();
+                var buffer = new Memory<byte>();
+                var bytes = await stream.ReadAsync(buffer);
+                var id = BitConverter.ToInt32(buffer.Span);
+                _clients.Add(id, client);
+            }
+        }
+
+        public async Task Send(int id, Texture2D data)
+        {
+            var colors = data.GetPixels32();
+            var bytes = new byte[colors.Length * 4];
+
+            for (var i = 0; i < colors.Length; i++)
+            {
+                bytes[i * 4] = colors[i].r;
+                bytes[i * 4 + 1] = colors[i].g;
+                bytes[i * 4 + 2] = colors[i].b;
+                bytes[i * 4 + 3] = colors[i].a;
+            }
+            
+            await using var stream = _clients[id].GetStream();
+            await stream.WriteAsync(bytes);
+        }
     }
 }
