bece9fde66b547af7227cf62316bd2047a6a1f3c
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2024-03-12 13:22:03 +0100

    DefaultState: first version of object commands implemented

--- Assets/Scripts/Networking/openIAExtension/States/DefaultState.cs
+++ Assets/Scripts/Networking/openIAExtension/States/DefaultState.cs
@@ -25,10 +25,13 @@ namespace Networking.openIAExtension.States
             switch (data[1])
             {
                 case Categories.Datasets.Reset:
+                {
                     ModelManager.Instance.ResetState();
                     SnapshotManager.Instance.ResetState();
                     return this;
+                }
                 case Categories.Datasets.LoadDataset:
+                {
                     var nameLength = BitConverter.ToInt32(data, 2);
                     var name = BitConverter.ToString(data, 6, nameLength);
                     if (ModelManager.Instance.ModelExists(name))
@@ -36,11 +39,15 @@ namespace Networking.openIAExtension.States
                         await Sender.Send(new ACK());
                         return new WaitingForServerACK(Sender, () => ModelManager.Instance.ChangeModel(name));
                     }
+
                     await Sender.Send(new NAK());
                     return this;
+                }
                 default:
+                {
                     Debug.LogError($"Unhandled Subcategory in Dataset Operation: {BitConverter.ToString(data, 1, 1)}");
                     return this;
+                }
             }
         }
 
@@ -49,15 +56,53 @@ namespace Networking.openIAExtension.States
             switch (data[1])
             {
                 case Categories.Objects.SetMatrix:
+                {
+                    var id = BitConverter.ToUInt64(data, 2);
+                    var matrix = new Matrix4x4();
+                    for (var i = 0; i < 16; i++)
+                    {
+                        matrix[i] = BitConverter.ToSingle(data, 10 + (i * 4));
+                    }
+                    MatrixToObject(id, matrix);
                     break;
+                }
                 case Categories.Objects.Translate:
+                {
+                    var id = BitConverter.ToUInt64(data, 2);
+                    var x = BitConverter.ToSingle(data, 10);
+                    var y = BitConverter.ToSingle(data, 14);
+                    var z = BitConverter.ToSingle(data, 18);
+
+                    TranslateObject(id, new Vector3(x, y, z));
                     break;
+                }
                 case Categories.Objects.Scale:
+                {
+                    var id = BitConverter.ToUInt64(data, 2);
+                    var x = BitConverter.ToSingle(data, 10);
+                    var y = BitConverter.ToSingle(data, 14);
+                    var z = BitConverter.ToSingle(data, 18);
+                    ScaleObject(id, new Vector3(x, y, z));
                     break;
+                }
                 case Categories.Objects.RotateQuaternion:
+                {
+                    var id = BitConverter.ToUInt64(data, 2);
+                    var x = BitConverter.ToSingle(data, 10);
+                    var y = BitConverter.ToSingle(data, 14);
+                    var z = BitConverter.ToSingle(data, 18);
+                    var w = BitConverter.ToSingle(data, 22);
+                    RotateObject(id, new Quaternion(x, y, z, w));
                     break;
+                }
                 case Categories.Objects.RotateEuler:
+                {
+                    var id = BitConverter.ToUInt64(data, 2);
+                    var axis = data[8];
+                    var amount = BitConverter.ToSingle(data, 9);
+                    RotateObject(id, axis, amount);
                     break;
+                }
             }
 
             return Task.FromResult<InterpreterState>(this);
@@ -78,6 +123,81 @@ namespace Networking.openIAExtension.States
             }
             
             return Task.FromResult<InterpreterState>(this);
+        }
+
+        private static void MatrixToObject(ulong id, Matrix4x4 matrix)
+        {
+            if (id != 0)
+            {
+                // my slicing plane is the tablet...
+                // how would this work?
+                Debug.LogWarning("The SlicingPlane is the Tablet. It cannot be changed.");
+                return;
+            }
+
+            var transform = ModelManager.Instance.CurrentModel.transform;
+            transform.SetPositionAndRotation(matrix.GetPosition(), matrix.rotation);
+            transform.localScale = matrix.lossyScale;
+        }
+
+        private static void TranslateObject(ulong id, Vector3 translation)
+        {
+            if (id != 0)
+            {
+                // my slicing plane is the tablet...
+                // how would this work?
+                Debug.LogWarning("The SlicingPlane is the Tablet. It cannot be changed.");
+                return;
+            }
+            
+            ModelManager.Instance.CurrentModel.transform.position += translation;
+        }
+
+        private static void ScaleObject(ulong id, Vector3 scale)
+        {
+            if (id != 0)
+            {
+                // my slicing plane is the tablet...
+                // how would this work?
+                Debug.LogWarning("The SlicingPlane is the Tablet. It cannot be changed.");
+                return;
+            }
+
+            ModelManager.Instance.CurrentModel.transform.localScale += scale;
+        }
+        
+        private static void RotateObject(ulong id, int axis, float value)
+        {
+            if (id != 0)
+            {
+                // my slicing plane is the tablet...
+                // how would this work?
+                Debug.LogWarning("The SlicingPlane is the Tablet. It cannot be changed.");
+                return;
+            }
+
+            var vec = axis switch
+            {
+                0 => new Vector3(1, 0, 0),
+                1 => new Vector3(0, 1, 0),
+                2 => new Vector3(0, 0, 1),
+                _ => throw new ArgumentException($"Invalid axis: {axis}")
+            };
+            
+            ModelManager.Instance.CurrentModel.transform.Rotate(vec, value);
+        }
+
+        private static void RotateObject(ulong id, Quaternion quaternion)
+        {
+            if (id != 0)
+            {
+                // my slicing plane is the tablet...
+                // how would this work?
+                Debug.LogWarning("The SlicingPlane is the Tablet. It cannot be changed.");
+                return;
+            }
+
+            ModelManager.Instance.CurrentModel.transform.rotation *= quaternion;
         }
     }
 }
