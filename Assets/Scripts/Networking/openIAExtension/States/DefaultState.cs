eccf9f9c693039e0ce36bc8c0eb0c7ec31c05eea
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2024-03-12 11:56:53 +0100

    DefaultState: created other methods but without implementation

--- Assets/Scripts/Networking/openIAExtension/States/DefaultState.cs
+++ Assets/Scripts/Networking/openIAExtension/States/DefaultState.cs
@@ -12,16 +12,23 @@ namespace Networking.openIAExtension.States
         public DefaultState(ICommandSender sender) : base(sender)
         {
         }
-        
+
+        public override Task<InterpreterState> Client(byte[] data)
+        {
+            var id = BitConverter.ToUInt64(data, 1);
+            // INFO id is entirely unused on the client
+            return Task.FromResult<InterpreterState>(this);
+        }
+
         public override async Task<InterpreterState> Datasets(byte[] data)
         {
             switch (data[1])
             {
-                case 0x0:
+                case Categories.Datasets.Reset:
                     ModelManager.Instance.ResetState();
                     SnapshotManager.Instance.ResetState();
                     return this;
-                case 0x1:
+                case Categories.Datasets.LoadDataset:
                     var nameLength = BitConverter.ToInt32(data, 2);
                     var name = BitConverter.ToString(data, 6, nameLength);
                     if (ModelManager.Instance.ModelExists(name))
@@ -29,15 +36,48 @@ namespace Networking.openIAExtension.States
                         await Sender.Send(new ACK());
                         return new WaitingForServerACK(Sender, () => ModelManager.Instance.ChangeModel(name));
                     }
-                    else
-                    {
-                        await Sender.Send(new NAK());
-                        return this;
-                    }
+                    await Sender.Send(new NAK());
+                    return this;
                 default:
                     Debug.LogError($"Unhandled Subcategory in Dataset Operation: {BitConverter.ToString(data, 1, 1)}");
                     return this;
             }
+        }
+
+        public override Task<InterpreterState> Objects(byte[] data)
+        {
+            switch (data[1])
+            {
+                case Categories.Objects.SetMatrix:
+                    break;
+                case Categories.Objects.Translate:
+                    break;
+                case Categories.Objects.Scale:
+                    break;
+                case Categories.Objects.RotateQuaternion:
+                    break;
+                case Categories.Objects.RotateEuler:
+                    break;
+            }
+
+            return Task.FromResult<InterpreterState>(this);
+        }
+
+        public override Task<InterpreterState> Snapshots(byte[] data)
+        {
+            switch (data[1])
+            {
+                case Categories.Snapshots.Create:
+                    break;
+                case Categories.Snapshots.Remove:
+                    break;
+                case Categories.Snapshots.Clear:
+                    break;
+                case Categories.Snapshots.SlicePosition:
+                    break;
+            }
+            
+            return Task.FromResult<InterpreterState>(this);
         }
     }
 }
