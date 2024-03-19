2687654ad728304914d2534b64a9fcf495b8fef3
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2024-03-19 09:41:20 +0100

    ModelIntersection: rewritten function to get normalized intersection positions to no longer create and destroy multiple gameobjects, tanking performance in the process

--- Assets/Scripts/Model/ModelIntersection.cs
+++ Assets/Scripts/Model/ModelIntersection.cs
@@ -24,19 +24,21 @@ namespace Model
         public IEnumerable<Vector3> GetNormalisedIntersectionPosition()
         {
             var intersectionPoints = GetIntersectionPoints();
-            var size = _modelBoxCollider.size;
-            var halfColliderSize = new Vector3(size.x / 2, size.y / 2, size.z / 2);
+            var halfColliderSize = _modelBoxCollider.size / 2;
 
-            var normalisedPositions = new List<Vector3>();
-            foreach (var p in intersectionPoints)
-            {
-                var tempGo = new GameObject("Temp");
-                tempGo.transform.localPosition = p;
-                tempGo.transform.SetParent(_model.transform);
-                var localPos = tempGo.transform.localPosition;
-                Object.Destroy(tempGo);
-                normalisedPositions.Add(GetNormalisedPosition(localPos, halfColliderSize));
-            }
+            var normalisedPositions = intersectionPoints
+                .Select(p => _model.transform.worldToLocalMatrix.MultiplyPoint(p))
+                .Select(newPosition => GetNormalisedPosition(newPosition, halfColliderSize));
+
+            // foreach (var p in intersectionPoints)
+            // {
+            //     var tempGo = new GameObject("Temp");
+            //     tempGo.transform.localPosition = p;
+            //     tempGo.transform.SetParent(_model.transform);
+            //     var localPos = tempGo.transform.localPosition;
+            //     Object.Destroy(tempGo);
+            //     normalisedPositions.Add(GetNormalisedPosition(localPos, halfColliderSize));
+            // }
 
             return CalculatePositionWithinModel(normalisedPositions, _modelBoxCollider.size);
         }
